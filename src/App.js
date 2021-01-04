61926b993e209472db981ab13d8eab83116a3b81
Btissameq <qorchi.btissam@gmail.com>
2021-01-04 23:13:02 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { FaReact } from "react-icons/fa";
+import { MdCode } from "react-icons/md";
+import { BsFillBrightnessLowFill } from "react-icons/bs";
+import { FcAutomatic } from "react-icons/fc";
+import { HiCode } from "react-icons/hi";
+import {IconContext} from "react-icons";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // pour avoir le même style 
+    <IconContext.Provider value={{color:'red',size:'25px'}}> 
+    <div className="App" co>
+      {/* Font awesome icon */}
+      <p style={{color:"gray",fontSize:"12px"}}>Font awesome icon</p>
+      <FaReact/>
+       {/* Material Design icons */}
+       <br/>
+       <p style={{color:"gray",fontSize:"12px"}}>Material Design icon</p>
+      <MdCode/>
+      <p style={{color:"gray",fontSize:"12px"}}>Bootstrap Icons</p>
+      <BsFillBrightnessLowFill/>
+      <p style={{color:"gray",fontSize:"12px"}}>Flat Color Icons</p>
+      <FcAutomatic/>
+      <p style={{color:"gray",fontSize:"12px"}}>Heroicons</p>
+      <HiCode/>
     </div>
+    </IconContext.Provider>
   );
 }
 
